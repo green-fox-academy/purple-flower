3e82a7a167492039560a6134fbf93dd1c24ae004
Lilla Virag <viraglilla@gmail.com>
2015-12-17 14:56:24 +0100

    save menu

--- week-6/game/character.py
+++ week-6/game/character.py
@@ -8,11 +8,31 @@ class Character():
 
     def random_status(self):
         self.dexterity = randint(1,6) + 6
-        self.health = 2 * randint(1,6) + 6
+        self.health = 2 * randint(1,6) + 12
         self.luck = randint(1,6) + 6
+
+    def strike(self):
+        self.current_health = self.health - 2
+
+    def roll(self):
+        self.roll_number = 2 * randint(1, 6)
+
+    def strike_strength(self):
+        self.roll()
+        return(self.roll_number + self.dexterity)
 
     def print_roll_status(self):
         print("dexterity: " + str(self.dexterity) + "\n" + "health: " + str(self.health) + "\n" + "luck: " + str(self.luck))
 
-    def print_character_status(self):
-        self.print_roll_status
+    def print_inventory_status(self):
+        print("inventory: " + self.inventory[0] + ", " + self.inventory[1] + ", " + self.inventory[2])
+
+    def print_start_status_character(self):
+        print("Name: " + self.name + "\n" + "Max health: " + str(self.health) + "\n" + "Max dexterity: " + str(self.dexterity) + "\n" + "Max luck: " + str(self.luck) + "\n")
+
+    def print_start_status_monster(self):
+        print("Monster name: " + self.name + "\n" + "Max health: " + str(self.health) + "\n" + "Max dexterity: " + str(self.dexterity) + "\n")
+
+    def create_dictionary(self):
+        dictionary = {'name' : self.name, 'max_dexterity' : self.dexterity, 'max_health' : self.health, 'max_luck' : self.luck, 'inventory' : [self.inventory[0], self.inventory[1], self.inventory[2]]}
+        return dictionary
--- week-6/game/game.py
+++ week-6/game/game.py
@@ -1,8 +1,22 @@
 from menu import Menu
 from menu_item import MenuItem
 from character import Character
+import json
+import os
 
 user = Character()
+monster = Character()
+
+
+def strike(self):
+    player = user.strike_strength()
+    ennemy = monster.strike_strength()
+    if player > ennemy:
+        print("You hit the monster!")
+    elif player < ennemy:
+        print("The monster hit you!")
+    after_strike_submenu.display_menu()
+
 
 def new_game():
     user_name = input("Please enter your name: ")
@@ -11,7 +25,7 @@ def new_game():
     new_game_submenu.display_menu()
 
 def load_game():
-    print("majd kesobb kitalalom")
+    pass
 
 def exit_game():
     exit()
@@ -21,25 +35,56 @@ def continue_newgame():
     user.print_roll_status()
     continue_submenu.display_menu()
 
-def save():
-    save_submenu.display_menu()
+def list_json_files():
+    json_list = []
+    for file in os.listdir():
+        if file.endswith(".json"):
+            json_list.append(file)
+    for item in json_list:
+        print(item.split('.')[0])
+    return json_list
 
+# def load():
+#     saved_games = []
+#     input_file = open("saved_games.json", "r")
+#     try:
+#         saved_games = json.load(saved_games)
+#     except Exception as e:
+#         pass
+#     input_file.close()
+    # return saved_games
+
+def save():
+    print("already saved items: ")
+    list_json_files()
+    print()
+    save_submenu.display_menu()
 
 def show_quit_submenu():
     quit_submenu.display_menu()
 
-def quit_submenu():
-    pass
+def save_and_quit():
+    json_file_name = input("Please enter your file name: ")
+    output_file = open(json_file_name + '.json', 'w')
+    json.dump(user.create_dictionary(), output_file)
+    output_file.close()
+    exit()
 
-def save_and_quit_submenu():
-    pass
+def save_rewrite_and_quit():
+    print("already saved items: ")
+    list_json_files()
+    print()
+    json_file_name = input("Please enter a file name from the list: ")
+    output_file = open(json_file_name + '.json', 'w')
+    json.dump(user.create_dictionary(), output_file)
+    output_file.close()
+    exit()
 
 def resume():
     pass
 
 def add_new_item():
     pass
-
 
 def continue_to_potion():
     print("Please choose a potion: ")
@@ -62,11 +107,28 @@ def luck_potion():
     potion_submenu.display_menu()
 
 def continue_begin():
+    print("The status of your character:")
+    user.print_roll_status()
+    user.print_inventory_status()
     begin_submenu.display_menu()
 
 def begin_game():
+    print("Test your sword in a test fight!" + "\n")
+    monster.name = "opi"
+    monster.random_status()
+    user.print_start_status_character()
+    monster.print_start_status_monster()
+    fight_submenu.display_menu()
+
+
+def new_strike():
+    fight_submenu.display_menu()
+
+def retreat():
     pass
 
+def try_luck():
+    pass
 
 main_menu = Menu([
     MenuItem(1, "New Game", new_game),
@@ -82,22 +144,23 @@ new_game_submenu = Menu([
 ])
 
 quit_submenu = Menu([
-    MenuItem(1, "Save and Quit", save_and_quit_submenu),
-    MenuItem(2, "Quit without save", quit_submenu),
+    MenuItem(1, "Save and Quit", save_and_quit),
+    MenuItem(2, "Quit without save", exit_game),
     MenuItem(3, "Resume", resume)
 ])
 
 save_submenu = Menu([
-    MenuItem(1, "Add new item", add_new_item),
-    MenuItem(2, "Resume", resume),
-    MenuItem(3, "Quit", quit_submenu)
+    MenuItem(1, "Add new item", save_and_quit),
+    MenuItem(2, "Rewrite a saved item", save_rewrite_and_quit),
+    MenuItem(3, "Resume", resume),
+    MenuItem(4, "Quit", show_quit_submenu)
 ])
 
 continue_submenu = Menu([
     MenuItem(1, "Reroll stats", continue_newgame),
     MenuItem(2, "Continue", continue_to_potion),
     MenuItem(3, "Save", save),
-    MenuItem(4, "Quit", quit_submenu)
+    MenuItem(4, "Quit", show_quit_submenu)
 ])
 
 choose_potion_submenu = Menu([
@@ -109,13 +172,27 @@ choose_potion_submenu = Menu([
 potion_submenu = Menu([
     MenuItem(1, "Reselect the Potion", continue_to_potion),
     MenuItem(2, "Continue", continue_begin),
-    MenuItem(3, "Quit", quit_submenu)
+    MenuItem(3, "Quit", show_quit_submenu)
 ])
 
 begin_submenu = Menu([
     MenuItem(1, "Begin", begin_game),
     MenuItem(2, "Save", save),
-    MenuItem(3, "Quit", quit_submenu)
+    MenuItem(3, "Quit", show_quit_submenu)
+])
+
+fight_submenu = Menu([
+    MenuItem(1, "Strike", strike),
+    MenuItem(2, "Retreat", retreat),
+    MenuItem(3, "Quit", show_quit_submenu)
+])
+
+
+after_strike_submenu = Menu([
+    MenuItem(1, "Continue", new_strike),
+    MenuItem(2, "Try your luck", try_luck),
+    MenuItem(3, "Retreat", retreat),
+    MenuItem(4, "Quit",show_quit_submenu),
 ])
 
 main_menu.display_menu()
